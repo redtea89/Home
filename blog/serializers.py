3b8fb55397a06bd757aa00f7be4ed963a8fca1f8
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-23 00:34:29 +0900

    DELETE: 기존 블로그 삭제

--- blog/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from blog.models import Blog
-
-class BlogSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Blog
-        fields = ['title','subtitle','text','tag','created','updated']
