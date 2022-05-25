22a3c4f81c30517782332f1b3b594af70e57a3e1
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-25 13:39:29 +0900

    FIX: index url 경로 변경

--- django_project/config/urls.py
+++ django_project/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('main.urls')),
+    path('', index),
 ]
--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('posts', views.PostListCreatetView.as_view(), name='index-list'),
     path('posts/<int:pk>', views.PostRetrieveUpdateDeleteView.as_view(), name='index-detail')
 ]
