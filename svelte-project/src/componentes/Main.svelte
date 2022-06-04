58f32e276d04142f94747a89238b1cf31c635148
YUN_SANGMIN <74187642+redtea89@users.noreply.github.com>
2022-06-04 19:43:00 +0900

    ADD: 재미있는 바탕화면

--- svelte-project/src/componentes/Main.svelte
+++ svelte-project/src/componentes/Main.svelte
@@ -1,11 +1,12 @@
 <script>
-    let name = "안녕하시오";
+	let src = '/tutorial/sample1.gif';
+	let name = 'Rick Astley';
 </script>
 
 <h1>Main Section</h1>
 <p>홈페이지 개발중입니다. v2</p>
 <p><a href="https://github.com/redtea89/Home">개발 Github Link</a></p>
-
+<img {src} alt="{name} dancing">
 <style>
     
 </style>
