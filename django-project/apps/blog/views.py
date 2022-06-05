ff144db17cf095b19eecf51ffe8bb8e44a7a0bbb
YUN_SANGMIN <74187642+redtea89@users.noreply.github.com>
2022-06-05 21:56:29 +0900

    CHORE: ACTION TEST

--- django-project/apps/blog/views.py
+++ django-project/apps/blog/views.py
@@ -8,7 +8,7 @@ from .serializers import *
 def index(request):
     url = "https://github.com/users/redtea89/projects/2/views/1"
     repo = "https://github.com/redtea89/Home"
-    return HttpResponse(f"<h1>[공사중...2] 홈페이지 리뉴얼중입니다.</h1><h4><a href={url}>프로젝트 진행과정 확인하기</a></h4><h4><a href={repo}>레포지터리</a></h4>")
+    return HttpResponse(f"<h1>[공사중...3] 홈페이지 리뉴얼중입니다.</h1><h4><a href={url}>프로젝트 진행과정 확인하기</a></h4><h4><a href={repo}>레포지터리</a></h4>")
 
 
 class PostListCreateView(ListCreateAPIView):
